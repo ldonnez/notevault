a2e5ee748ca563a9877959ba81f709aea30586fa
Lenny <lenny.donnez@gmail.com>
2025-11-20 21:08:16 +0100

    feat: load config
    Ensures sensible defaults are present when no config file is found.

--- nv.sh
+++ nv.sh
@@ -21,6 +21,47 @@ Commands:
 EOF
 }
 
+###############################################################################
+# Setup
+###############################################################################
+
+# Set default global variables
+# Variables prefixed with _ should not be overriden.
+_set_default_values() {
+  : "${REPO_ROOT:=$(git rev-parse --show-toplevel)}"
+  : "${GPG_RECIPIENTS:=}"
+  : "${_CONFIG_FILE:=$REPO_ROOT/.note-vault-config}"
+  : "${PLAINDIR:=notes}"
+  : "${ENCRYPTEDDIR:=encrypted}"
+}
+
+# Initializes $PLAINDIR, $ENCRYPTEDDIR
+_setup_directories() {
+  # Create directories if not exist
+  mkdir -p "$PLAINDIR"
+  mkdir -p "$ENCRYPTEDDIR"
+}
+
+_load_config() {
+  # Ensure we're inside a git repo
+  if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    printf "not inside a git repository.\n"
+    exit 1
+  fi
+
+  _set_default_values
+
+  # Load repo-specific config
+  if [ -f "$_CONFIG_FILE" ]; then
+    # shellcheck source=/dev/null
+    source "$_CONFIG_FILE"
+  fi
+
+  _setup_directories
+
+  : "${ARCHIVE:=$ENCRYPTEDDIR/$PLAINDIR.tar.gz.gpg}"
+}
+
 _parse_args() {
   while [ $# -gt 0 ]; do
     case "$1" in
@@ -49,6 +90,7 @@ _parse_args() {
 
 # Entrypoint
 main() {
+  _load_config
   _parse_args "$@"
 }
 
---
+++ test/setup_directories_test.bats
@@ -0,0 +1,28 @@
+#!/usr/bin/env bats
+
+setup() {
+  bats_load_library bats-support
+  bats_load_library bats-assert
+
+  TEMP="$(mktemp -d)"
+
+  # shellcheck source=nv.sh
+  source "nv.sh"
+}
+
+teardown() {
+  rm -rf "$TEMP"
+}
+
+@test "Creates missing directories" {
+  # Run in subshell to prevent collision in other tests
+  (
+    local PLAINDIR="$TEMP/test_plain"
+    local ENCRYPTEDDIR="$TEMP/test_encrypted"
+
+    _setup_directories
+
+    assert_equal "" "$([[ -d "$PLAINDIR" ]])"
+    assert_equal "" "$([[ -d "$ENCRYPTEDDIR" ]])"
+  )
+}
